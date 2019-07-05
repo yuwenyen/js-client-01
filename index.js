24e766219ad312c9c86329fd59858193607a18ae
Yuwen <u06m3jp600@gmail.com>
2019-07-05 09:25:49 +0900

    7/5 課題

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // ここにJavaScriptコードを書く
+const tarea = document.getElementById('tarea');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+
+btn.addEventListener('click',()=>{
+    let findtxt = input1.value;
+    let reptxt = input2.value;
+    let tagtxt = tarea.value;
+
+    findtxt = new RegExp(findtxt, 'g');
+    tagtxt = tagtxt.replace(findtxt, reptxt);
+
+    result.innerText = tagtxt;
+});
